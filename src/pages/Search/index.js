1ee5514961cf52bbf91db93508fd91d0925d3119
matheusrsdeveloper <156608729+matheusrsdeveloper@users.noreply.github.com>
2026-07-02 16:35:54 -0300

    Criando components SearchVideoList

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,9 +1,9 @@
 import Container from "../../components/Container";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
-import VideoList from "../../components/VideoList";
 import style from "./Search.module.css";
 import videos from "../../json/videos.json";
+import SearchVideoList from "../../components/SearchVideoList";
 
 function Search() {
     return (
@@ -12,7 +12,8 @@ function Search() {
            <Container>
                 <section className={style.search}>
                     <h2>Pesquisar</h2>
-                    <VideoList videos={videos} />
+
+                    <SearchVideoList videos={videos} />
                 </section>
            </Container>
            <Footer/>
